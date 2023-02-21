0a20b561ca32644afec6abe25f65d854282f94dd
dnyaneshwar2023 <dnyaneshwarw@sahaj.ai>
2023-02-21 15:30:10 +0530

    Add esop type in body for sell orders

--- scripts/createOrder.js
+++ scripts/createOrder.js
@@ -17,32 +17,35 @@ submitButton.onclick = (e) => {
     console.log(price)
     console.log(quantity)
     console.log(type)
-    var body = {}
+    var body = {
+        price,
+        quantity,
+        type,
+    }
 
     if (type == "SELL") {
-        body = {
-            price: price,
-            quantity: quantity,
-            type: type
-        }
-    }
-    else {
-        body = {
-            price: price,
-            quantity: quantity,
-            type: type,
-            esopType: esopType
-        }
-    }
+       body["esopType"] = esopType
+    }   
 
     fetch("http://localhost:8080/user/amaan/order", {
         method: "POST",
-        body: JSON.stringify(body),
+        body: JSON.stringify({...body}),
         headers: {
             "Content-type": "application/json"
         }
     }).then(response => response.json().then(res => {
         console.log(res)
+        
+        if(response.status == 400) {
+            return alert("Invalid Request")
+        }
+        if(res.errors == undefined) {
+            alert("Order Placed Successfully")
+        }
+        else
+        {
+            alert(res.errors)
+        }
     }))
 
     e.preventDefault()
