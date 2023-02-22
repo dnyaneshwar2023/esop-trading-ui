518a694245d9baf32d352088a4a60bac0c7a698b
dnyaneshwar2023 <dnyaneshwarw@sahaj.ai>
2023-02-22 23:28:05 +0530

    Extract getOrderHistory to file

---
+++ src/scripts/getOrders.js
@@ -0,0 +1,11 @@
+const getOrderHistory = (userName) => {
+    return fetch(`http://localhost:8080/user/${userName}/order`, {
+        method: "GET"
+    }).then(response => response.json().then(res => {
+        return res;
+    })).catch(err => {
+        return [];
+    })
+}
+
+export default getOrderHistory;
