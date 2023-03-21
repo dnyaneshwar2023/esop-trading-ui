d7e2fdada2479e18c8e224154df04dca44830bc6
dnyaneshwar2023 <dnyaneshwarw@sahaj.ai>
2023-03-21 12:46:31 +0530

    Add test for creating order

--- __tests__/CreateOrderService.test.js
+++ __tests__/CreateOrderService.test.js
@@ -4,6 +4,24 @@ import CreateOrderService from "../src/scripts/CreateOrderService"
 describe("Create Order Tests", () => {
     let createOrderService = new CreateOrderService()
 
+    it("should create an order", async () => {
+        let body = { price: 10, quantity: 10, type: "BUY" }
+        let mockResponse = { orderId: 1, price: 10, quantity: 10, type: "BUY" }
+
+        const mockUser = "dnyaneshwar";
+
+        global.fetch = jest.fn().mockImplementation(() => {
+            return Promise.resolve({
+                json: () => Promise.resolve(mockResponse)
+            });
+        });
+
+        let response = await createOrderService.placeOrder(body, mockUser)
+
+        expect(response).toEqual(mockResponse)
+
+    });
+
     it("should throw an error if order placement fails", async () => {
         const body = { price: 10, quantity: 10, type: "BUY" };
         const userName = "dnyaneshwar";
@@ -14,5 +32,5 @@ describe("Create Order Tests", () => {
         await expect(createOrderService.placeOrder(body, userName)).rejects.toThrow(error);
     });
 
-    
+
 })
