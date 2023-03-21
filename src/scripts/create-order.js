b1b6e12321dfec093aafe42d634623bddf0c0534
dnyaneshwar2023 <dnyaneshwarw@sahaj.ai>
2023-03-21 11:18:21 +0530

    Extract body creation method to function

--- src/scripts/create-order.js
+++ src/scripts/create-order.js
@@ -1,19 +1,16 @@
 import "../css/index.css"
 
 import '../css/create-order.css'
+import CreateOrderService from "./CreateOrderService"
 
 const submitButton = document.getElementById("submit")
 
-submitButton.onclick = async (e) => {
+function generateApiPayload() {
     const price = document.getElementById("price").value
     const quantity = document.getElementById("quantity").value
     const type = document.getElementById("selector").value
     var esopType = document.getElementById("esoptype").value
-    var userName = document.getElementById("username").value
 
-    console.log(price)
-    console.log(quantity)
-    console.log(type)
     var body = {
         price: parseInt(price),
         quantity: parseInt(quantity),
@@ -22,9 +19,23 @@ submitButton.onclick = async (e) => {
 
     if (type == "SELL") {
        body["esopType"] = esopType
-    }   
+    }
 
+    return body
+}
+
+submitButton.onclick = async (e) => {
     e.preventDefault()
+
+    var userName = document.getElementById("username").value
+
+    let createOrderService = new CreateOrderService()
+    
+    let response = createOrderService.placeOrder(generateApiPayload(), userName)
+
+    console.log(response)
+    console.log("HEre")
+
 
 }
 
