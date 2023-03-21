f1ff335e90c3ab7e48a13c4f934f6bbb6fdee332
dnyaneshwar2023 <dnyaneshwarw@sahaj.ai>
2023-03-21 12:37:01 +0530

    Add test for order creation failure

--- __tests__/CreateOrderService.test.js
+++ __tests__/CreateOrderService.test.js
@@ -1,30 +1,18 @@
+
 import CreateOrderService from "../src/scripts/CreateOrderService"
-import OrderHistoryService from "../src/scripts/OrderHistoryService"
-import CreateBuyOrderSuccess from "../stubs/CreateBuyOrderSuccess"
 
 describe("Create Order Tests", () => {
     let createOrderService = new CreateOrderService()
 
-    it("should return error given insufficient amount in wallet for buy order", async () => {
-        let response = await createOrderService.placeOrder({
-            price: 10,
-            quantity: 10,
-            type: "BUY"
-        }, "amaan")
-
-        expect(response).toEqual(["Insufficient amount in wallet"])
-    })
-
-    it("should create buy order", async() => {
-        let buyorderService = new CreateBuyOrderSuccess()
-
-        let response = JSON.parse(await buyorderService.placeOrder({price: 10,
-            quantity: 10,
-            type: "BUY"}, "amaan"))
-        
-        expect(response.price).toEqual(10)
-
-    })
+    it("should throw an error if order placement fails", async () => {
+        const body = { price: 10, quantity: 10, type: "BUY" };
+        const userName = "dnyaneshwar";
+        const error = new Error("Failed to place order");
+        global.fetch = jest.fn().mockImplementation(() => {
+            return Promise.reject(error);
+        });
+        await expect(createOrderService.placeOrder(body, userName)).rejects.toThrow(error);
+    });
 
     
 })
