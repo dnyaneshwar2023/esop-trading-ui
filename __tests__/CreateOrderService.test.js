80fc78b5ea47a30f147a20a0887727ecb0021427
Vinayak Gaikwad <vinayakg@sahaj.ai>
2023-03-21 15:43:19 +0530

    Add asserts for create order test

--- __tests__/CreateOrderService.test.js
+++ __tests__/CreateOrderService.test.js
@@ -4,17 +4,28 @@ describe("Create Order Tests", () => {
     it("should create an order", async () => {
         let body = { price: 10, quantity: 10, type: "BUY" };
         let mockResponse = { orderId: 1, price: 10, quantity: 10, type: "BUY" };
-
         const mockUser = "dnyaneshwar";
 
-        global.fetch = jest.fn().mockImplementation(() => {
+        const mockFetch = jest.fn().mockImplementation(() => {
             return Promise.resolve({
                 json: () => Promise.resolve(mockResponse),
             });
         });
+        global.fetch = mockFetch;
 
         let response = await placeOrder(body, mockUser);
 
+        expect(mockFetch).toHaveBeenCalledWith(
+            `http://localhost:8080/user/${mockUser}/order`,
+            {
+                method: "POST",
+                body: JSON.stringify({ ...body }),
+                headers: {
+                    "Content-type": "application/json",
+                },
+            }
+        );
+        expect(mockFetch).toHaveBeenCalledTimes(1)
         expect(response).toEqual(mockResponse);
     });
 
